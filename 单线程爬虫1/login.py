79c0e15b0028aa572b6111cea348e5e6e060d409
zhuzhu <jiniansiren@gmail.com>
2017-07-26 17:40:33 +0800

    单线程爬虫1

--- 单线程爬虫1/login.py
+++ 单线程爬虫1/login.py
@@ -3,11 +3,19 @@
 import requests
 # 导入requests模块，该模块
 import codecs
-#导入codecs模块，
+
+# 导入codecs模块，解决编码问题
 zhuanqu = requests.get('http://cuiqingcai.com')
+#抓取所有web内容
 print zhuanqu.encoding
+#打印抓取web的编码格式
 print zhuanqu.cookies
+#打印抓取web的cookies
 html = zhuanqu.text
+#定义HTML 等于抓取页面的内容
 file = codecs.open('2.txt', 'w', 'utf-8')
+#定义文件file
 file.write(html)
+#将抓取的内容写入文件
 file.close()
+#关闭文件
