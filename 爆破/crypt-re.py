ea208d572e5ec454a26a65aa80c8b103c76239cf
zhuzhu <jiniansiren@gmail.com>
2017-08-01 10:31:43 +0800

    clear
    Signed-off-by: zhuzhu <jiniansiren@gmail.com>

--- 爆破/crypt-re.py
+++ 爆破/crypt-re.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+#请注意  crypt模块仅支持linux平台，所以win下无法运行
+#请注意  txt中win下和linux下的不同。
 import crypt
 
 def testPass(cryptPass):
@@ -7,10 +9,12 @@ def testPass(cryptPass):
     dictFile = open('dictionary.txt', 'r')
     for word in dictFile.readlines():
         word = word.strip('\n')
-        cryptWord = crypt.crypt(word,salt)
+        print "匹配字典中的: " +word
+        cryptWord = crypt.crypt(word, salt)
+        print "字典生成的哈希值为 ：" +str(cryptWord)
         if (cryptWord == cryptPass):
             print "[+] Found Password: " +word+"\n"
-            return
+            # return
     print "[-] Password Not Found. \n"
     return
 
@@ -19,11 +23,13 @@ def main():
     for line in passFile.readlines():
         if ":" in line:
             user = line.split(':')[0]
+            print "爆破密码的用户名是: " +user
             cryptPass = line.split(':')[1].strip(' ')
+            print "爆破密码的哈希值为: " +cryptPass
             print "[*] Cracking Password For: "+user
             testPass(cryptPass)
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     main()
 
 
